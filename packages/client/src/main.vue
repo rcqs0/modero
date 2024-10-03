7c703baa45f22da80e209a0ef021ad2a7cafb391
Roderick Snel <rcq.snel@gmail.com>
2024-10-04 00:18:24 +0200

    fix

--- packages/client/src/main.vue
+++ packages/client/src/main.vue
@@ -4,9 +4,10 @@
 
 <style lang="postcss">
 html {
-  font-family: Inter var, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+  font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
     Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji,
     Segoe UI Symbol;
   font-size: 14px;
+  line-height: normal;
 }
 </style>
