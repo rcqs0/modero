fc6a6621a35181d3e4d721d5424e771537e284b8
Roderick Snel <rcq.snel@gmail.com>
2024-11-29 17:05:24 +0100

    fix

--- packages/server/src/sync.ts
+++ packages/server/src/sync.ts
@@ -59,7 +59,6 @@ export function connect(ws: WebSocket, req: IncomingMessage) {
 
       const message = encoding.toUint8Array(encoder)
       connections.forEach((_, ws) => ws.send(message))
-      console.log(Array.from(connections.keys()).indexOf(_origin), doc.toJSON())
     })
 
     awareness.on(
