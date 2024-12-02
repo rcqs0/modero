4a4a99a3831bb984123ccaf305028caa6a4cb6db
Roderick Snel <rcq.snel@gmail.com>
2024-12-02 23:11:07 +0100

    poc

--- packages/client/src/router.ts
+++ packages/client/src/router.ts
@@ -82,6 +82,10 @@ const router = createRouter({
       path: '/profiles',
       component: () => import('./pages/profiles.vue'),
     },
+    {
+      path: '/doc',
+      component: () => import('./pages/doc.vue'),
+    },
   ],
 })
 
